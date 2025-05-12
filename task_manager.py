fa1f650c09029a6616143821d99ac5b66b5b3b2a
Shingai Dzinotyiweyi <109967579+C-CREAD@users.noreply.github.com>
2025-05-12 19:12:42 +0200

    Updating files

--- task_manager.py
+++ task_manager.py
@@ -485,4 +485,4 @@ def statistics():
                            user_information=user_information)
 
 if __name__ == "__main__":
-    app.run(debug=False, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=int(3000))
